1c23cbd0bf09f95861b9a147d15bb4ea258dca0f
massimo <massimo@info.nl>
2019-04-25 18:20:21 +0200

    Simple Future composition

--- akkademy-db/src/test/scala/com/akkademy/pong/PongActorTest.scala
+++ akkademy-db/src/test/scala/com/akkademy/pong/PongActorTest.scala
@@ -3,11 +3,13 @@ package com.akkademy.pong
 import akka.actor.{ActorSystem, Props}
 import akka.util.Timeout
 import akka.pattern.ask
+import akka.testkit.TestActor.Message
 import org.scalatest.{FunSpecLike, Matchers}
 
-import scala.concurrent.Await
+import scala.concurrent.{Await, ExecutionContext, Future}
 import scala.concurrent.duration._
-
+import ExecutionContext.Implicits.global
+import scala.util.{Failure, Success}
 /**
   * Created by massimo.biancalani on 2019-04-25.
   *
@@ -18,6 +20,8 @@ class PongActorTest extends FunSpecLike with Matchers {
   implicit val timeout = Timeout(5 seconds)
 
   val pongActor = system.actorOf(Props(classOf[ActorPong]))
+
+  def askPong (message: String): Future[String] = (pongActor ? message).mapTo[String]
 
   describe("pong actor") {
     it("should respond with pong") {
@@ -31,5 +35,20 @@ class PongActorTest extends FunSpecLike with Matchers {
         Await.result(future.mapTo[String], 1 second)
       }
     }
+    it ("should print to console") {
+      askPong("ping").onComplete {
+        case Success(answer) => println("replied with: " + answer)
+        case Failure(t) => println("An error has occurred: " + t.getMessage)
+      }
+    }
+
+    it ("should transform the result") {
+      askPong("ping").map(x => x.charAt(0)) onComplete {
+        case Success(s) => println (s"Mapped with ${s}")
+        case Failure(t) => println("An error has occurred: " + t.getMessage)
+      }
+    }
+
+    
   }
 }
